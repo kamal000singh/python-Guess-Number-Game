a8a06c37bd6f6c35315711c2a538aa92d731f47e
kamal singh rawat <kamal000rawat@gmail.com>
2020-09-26 12:38:20 +0530

    Add files via upload

---
+++ guess_number_game.py
@@ -0,0 +1,20 @@
+import random
+n = random.randint(1,100)
+count=1
+print("Guess your number (1-100)")
+print("You have only 9 attempt ")
+while(True):
+    a=int(input("Enter your guess "))
+    if(a==n):
+        remain = 9 - count
+        print("you guess correct \nGuess attempt :",count,"Remaining:",remain)
+        break
+    elif(a<n):
+        print("guess greater number")
+        count=count + 1
+    elif(count==9):
+        print("GAME OVER")
+        break
+    else:
+        print("guess lesser number")
+        count = count + 1
